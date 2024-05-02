4af077d848a4adb85bef15709e6f7cf23b4b0b9a
Erik Hedenström <50584942+444Erik@users.noreply.github.com>
2024-05-02 21:58:47 +0200

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -23,7 +23,7 @@ export default function Root() {
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
-        <div class="bg">srfcjbdkfjdfdvfnjkdf vnjknkdfkdf</div>
+        <div class="bg"></div>
         <Suspense>
           <ErrorBoundary>
             <Routes>
